cda439975488d26ab4460f5d02db0df3940c218e
tushar27rajput <tushar.rajput774@gmail.com>
2018-04-12 16:14:33 +0530

    notepad

---
+++ src/swd1/Autoswd44.java
@@ -0,0 +1,79 @@
+package swd1;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+public class Autoswd44 
+{
+
+	public static void main(String[] args) throws IOException, InterruptedException, AWTException
+	{
+		
+		Runtime.getRuntime().exec("Notepad.exe");
+		Thread.sleep(5000);
+		
+		Robot r= new Robot();
+		
+		
+		r.keyPress(KeyEvent.VK_ALT);
+		r.keyPress(KeyEvent.VK_SPACE);
+		r.keyPress(KeyEvent.VK_X);
+		r.keyRelease(KeyEvent.VK_X);
+		r.keyRelease(KeyEvent.VK_SPACE);
+		r.keyRelease(KeyEvent.VK_ALT);
+		Thread.sleep(5000);
+		
+		//Create New File
+		r.keyPress(KeyEvent.VK_ALT);
+		r.keyPress(KeyEvent.VK_F);
+		r.keyRelease(KeyEvent.VK_F);
+		r.keyRelease(KeyEvent.VK_ALT);
+		Thread.sleep(5000);
+		
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		Thread.sleep(5000);
+		
+		//Send Data to Notepad
+		StringSelection x=new StringSelection("Welcome To Jalgaon");
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(x, null);
+		
+		r.keyPress(KeyEvent.VK_CONTROL);
+		r.keyPress(KeyEvent.VK_V);
+		r.keyRelease(KeyEvent.VK_V);
+		r.keyRelease(KeyEvent.VK_CONTROL);
+		Thread.sleep(5000);
+		
+		
+		r.keyPress(KeyEvent.VK_CONTROL);
+		r.keyPress(KeyEvent.VK_S);
+		r.keyRelease(KeyEvent.VK_S);
+		r.keyRelease(KeyEvent.VK_CONTROL);
+		Thread.sleep(5000);
+		
+		//Save the File
+		
+		StringSelection ss_1=new StringSelection("E:\\swd\\tushar1.txt");
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss_1, null);
+		
+		r.keyPress(KeyEvent.VK_CONTROL);
+		r.keyPress(KeyEvent.VK_V);
+		r.keyRelease(KeyEvent.VK_V);
+		r.keyRelease(KeyEvent.VK_CONTROL);
+		Thread.sleep(5000);
+		
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		Thread.sleep(5000);
+
+		r.keyPress(KeyEvent.VK_ALT);
+		r.keyPress(KeyEvent.VK_F4);
+		r.keyRelease(KeyEvent.VK_F4);
+		r.keyRelease(KeyEvent.VK_ALT);
+	}
+
+}
